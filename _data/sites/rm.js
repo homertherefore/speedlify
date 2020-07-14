5a3ecdb8e19e8bb8d298d2005eaa205a89b61eaf
Homer <sean.homer@therefore.ca>
2020-07-14 07:44:36 -0400

    * Testing RM run on netlify with short refresh (5mins)

--- _data/sites/rm.js
+++ _data/sites/rm.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: "Rocky Mountaineer", // optional, falls back to object key
 	description: "RM Performance Audits",
 	options: {
-		frequency: 60 * 23, // 24 hours
+		frequency: 5, //60 * 23, // 24 hours
 		freshChrome: "site",
 		// if the sites don’t share assets on the same domain, we can reset
 		// chrome with each run instead of each site in every run (it’s faster)
